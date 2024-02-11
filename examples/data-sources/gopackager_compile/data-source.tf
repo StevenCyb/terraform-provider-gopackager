2408a687f57503571e32d2c3436ec3260a20970d
Steven Cybinski <41087154+StevenCyb@users.noreply.github.com>
2024-02-11 22:30:37 +0100

    doc: typo (#8)

--- examples/data-sources/gopackager_compile/data-source.tf
+++ examples/data-sources/gopackager_compile/data-source.tf
@@ -24,22 +24,22 @@ output "example" {
     # `output_path` provides the path and file name of the compiled binary.
     # If `zip = true`, this will refer to the zip file.
     output_path = data.gopackager_compile.example.output_path
-    # `output_md5` provides the md5 hash of the compiled binary.
+    # `output_md5` provides the md5 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_md5 = data.gopackager_compile.example.output_md5
-    # `output_sha1` provides the SHA1 hash of the compiled binary.
+    # `output_sha1` provides the SHA1 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_sha1 = data.gopackager_compile.example.output_sha1
-    # `output_sha256` provides the SHA256 hash of the compiled binary.
+    # `output_sha256` provides the SHA256 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_sha256 = data.gopackager_compile.example.output_sha256
-    # `output_sha512` provides the SHA512 hash of the compiled binary.
+    # `output_sha512` provides the SHA512 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_sha512 = data.gopackager_compile.example.output_sha512
-    # `output_sha256_base64` provides the Base64 ecndoded SHA256 hash of the compiled binary.
+    # `output_sha256_base64` provides the Base64 encoded SHA256 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_sha256_base64 = data.gopackager_compile.example.output_sha256_base64
-    # `output_sha512_base64` provides the Base64 ecndoded SHA512 hash of the compiled binary.
+    # `output_sha512_base64` provides the Base64 encoded SHA512 hash of the compiled binary or compressed ZIP file.
     # There are multiple factors that can affect the hash, that means
     output_sha512_base64 = data.gopackager_compile.example.output_sha512_base64
   }
